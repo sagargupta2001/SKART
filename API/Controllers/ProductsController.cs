cb2053389847dc6f0bf36e568d845d64661d6d6f
Sagar <sagargupta201@outlook.com>
2022-02-06 16:07:05 +0530

    Added controller for Products

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public ActionResult<Product> GetProducts()
+        {
+            var products = _storeContext.Products;
+            return Ok(products);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _storeContext.Products.Find(id);
+        }
+
+        
+    }
+}
