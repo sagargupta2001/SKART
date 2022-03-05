7a106b7791bb52e909f67ff75dabde04208ad51a
Sagar <sagargupta201@outlook.com>
2022-03-05 13:18:16 +0530

    Created basket entity

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -29,7 +29,11 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _storeContext.Products.FindAsync(id);
+            var product = await _storeContext.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
         }
 
         
---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Entities
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        // Navigation Properties
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
