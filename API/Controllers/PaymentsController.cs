48ab1499390910cb64423210d202c28bd8abd654
Sagar <sagargupta201@outlook.com>
2022-04-07 14:26:32 +0530

    Added submit payment functionality.

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -13,11 +17,12 @@ namespace API.Controllers
     {
         private readonly PaymentService _paymentService;
         private readonly StoreContext _context;
-
-        public PaymentsController(PaymentService paymentService, StoreContext context)
+        private readonly IConfiguration _config;
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
         {
-            _paymentService = paymentService;
+            _config = config;
             _context = context;
+            _paymentService = paymentService;
         }
 
         [Authorize]
@@ -46,6 +51,24 @@ namespace API.Controllers
             return basket.MapBasketToDto();
         }
 
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
 
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+                _config["StripeSettings:WhSecret"]);
+
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(x =>
+                x.PaymentIntentId == charge.PaymentIntentId);
+
+            if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
+
+            await _context.SaveChangesAsync();
+
+            return new EmptyResult();
+        }
     }
 }
